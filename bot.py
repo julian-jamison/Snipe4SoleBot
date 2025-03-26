e460a7bb7390d96501ed1a8e608f8bc6746ae6cc
julian-jamison <107572673+julian-jamison@users.noreply.github.com>
2025-03-26 09:42:36 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -243,8 +243,9 @@ if __name__ == "__main__":
 
     # Clean up stale lock files on reboot or crash
     for lock_file in [STARTUP_LOCK_FILE, TELEGRAM_LOCK_FILE]:
-        if os.path.exists(lock_file):
-            os.remove(lock_file)
+      # Clean up stale lock file for Telegram only
+        if os.path.exists(TELEGRAM_LOCK_FILE):
+            os.remove(TELEGRAM_LOCK_FILE)
 
     send_startup_message_once()
     Thread(target=bot_main_loop, daemon=True).start()

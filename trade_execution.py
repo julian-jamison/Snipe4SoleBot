ae70a3786c0f49b71cccfee6620f7abf9b3a4d81
julian-jamison <107572673+julian-jamison@users.noreply.github.com>
2025-03-28 12:36:01 -0700

    Update trade_execution.py

--- trade_execution.py
+++ trade_execution.py
@@ -10,7 +10,7 @@ from portfolio import add_position, remove_position, get_position, get_all_posit
 # from ai_prediction import predict_market_trend
 
 trade_settings = config["trade_settings"]
-export BACKTEST_MODE=false
+BACKTEST_MODE = False
 MOCK_DATA_FILE = "mock_pools.json"
 
 DEX_APIS = {

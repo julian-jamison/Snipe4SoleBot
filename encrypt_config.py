bc8cbed1b99ebf56287274ebd5406c89226ee32b
julian-jamison <107572673+julian-jamison@users.noreply.github.com>
2025-04-03 11:40:56 -0700

    Update encrypt_config.py

--- encrypt_config.py
+++ encrypt_config.py
@@ -6,7 +6,7 @@ import os
 raw_key = os.environ.get("CONFIG_ENCRYPTION_KEY")
 if not raw_key:
     raise EnvironmentError("❌ CONFIG_ENCRYPTION_KEY is not set in the environment.")
-ENCRYPTION_KEY = base64.urlsafe_b64decode(raw_key)
+ENCRYPTION_KEY = bytes.fromhex(raw_key)
 
 CONFIG_FILE = "config.json"
 ENCRYPTED_FILE = "config.enc"
